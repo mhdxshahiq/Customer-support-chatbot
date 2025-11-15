efe525f3278ce35c7eea2e75b226506144fe97bf
shahiq <88590609+mhdxshahiq@users.noreply.github.com>
2025-11-15 18:56:24 +0530

    Implement department prediction and logging
    Added department classification and logging functionality.

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,7 @@
 import os
+import joblib
+import json
+from datetime import datetime
 from dotenv import load_dotenv
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -8,6 +11,11 @@ from langchain_core.runnables import RunnableMap, RunnableSequence
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
+
+#Load models
+clf = joblib.load("models/department_classifier.pkl")
+vectorizer = joblib.load("models/vectorizer.pkl")
+
 
 model = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
@@ -26,6 +34,38 @@ db = Chroma(
 retriever = db.as_retriever(search_kwargs={"k": 3})
 
 chat_history = []
+
+def predict_department(text):
+    x_vec = vectorizer.transform([text])
+    prediction = clf.predict(x_vec)[0]
+    return prediction
+
+def save_routing_to_json(user_query, department):
+    os.makedirs("logs", exist_ok=True)
+    log_file = "logs/routing_log.json"
+
+    # Load old logs
+    if os.path.exists(log_file):
+        with open(log_file, "r") as f:
+            logs = json.load(f)
+    else:
+        logs = []
+
+    # Create new entry
+    entry = {
+        "timestamp": datetime.now().isoformat(),
+        "query": user_query,
+        "department": department
+    }
+
+    logs.append(entry)
+
+    # Save updated logs
+    with open(log_file, "w") as f:
+        json.dump(logs, f, indent=4)
+
+
+
 
 
 prompt = ChatPromptTemplate.from_messages([
@@ -63,8 +103,13 @@ print("Customer support chatbot. How can I help you?")
 while True:
     query = input("YOU: ")
     if query.lower() in ["exit", "quit", "bye"]:
-        print("BOT: Goodbye!")
+        print("BOT: Goodbye")
         break
+    
+    #backend log
+    department = predict_department(query)
+    print(f"[BACKEND ROUTING] Classified as → {department}")
+    save_routing_to_json(query, department)
 
     # docs = retriever.invoke(query)
 
@@ -74,6 +119,8 @@ while True:
     #     print("ID:", d.metadata.get("ticket_id", "N/A"))
     #     print("Content:", d.page_content[:300], "...")
 
+
+    # RAG
     response = qa_chain.invoke({
         "question": query,
         "history": "\n".join(chat_history)
@@ -85,3 +132,9 @@ while True:
     # Store chat history
     chat_history.append(f"User: {query}")
     chat_history.append(f"Bot: {bot_reply}")
+
+
+
+
+
+
